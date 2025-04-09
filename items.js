79e504d2e9c5d2ec4b6c700ded33d21f8f65f6bb
hello-connect <140159869+hello-connect@users.noreply.github.com>
2025-04-09 13:14:50 +0700

    Update items.js

--- items.js
+++ items.js
@@ -3,6 +3,7 @@ const items = [
     {id: 2, name: "Item Two", category: "main", price: 7.5},
     {id: 3, name: "Item Three", category: "snack", price: 12},
     {id: 4, name: "Item Four", category: "dessert", price: 8},
+{id: 5, name: "Item Five", category: "salads", price: 5},
 ]
 
 export { items }
