e9485c5c704398674d004ed9b6d0ef220a5b84ca
ndreyyy <ndreyyy@localhost.localdomain>
2025-04-09 20:05:51 +0700

    init

---
+++ items.js
@@ -0,0 +1,8 @@
+const items = [
+    {id: 1, name: "Item One", category: "main", price: 10},
+    {id: 2, name: "Item Two", category: "main", price: 7.5},
+    {id: 3, name: "Item Three", category: "snack", price: 12},
+    {id: 4, name: "Item Four", category: "dessert", price: 8},
+]
+
+export { items }
